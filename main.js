3603fcf32b2731528705a6687745e096245fee76
Adrian <adriansalvadortrabajos@gmail.com>
2024-05-20 16:04:17 +0100

    done

--- main.js
+++ main.js
@@ -1,33 +1,53 @@
-
-let board = document.getElementById("canvas");
+let board = document.getElementById('canvas')
+let screen = document.getElementById('start')
+let button = document.getElementById('start-button')
 //Player
-let player = new Player(225, 750);
-let timeMovePlayer
+let player = new Player(400, 550)
+let timerMovePlayer
+
 //Enemy
-let enemy = new Enemy(255, 0);
-let timeMoveEnemy
+let enemyArray = []
 
+let timerCreateEnemy
 
-function startGame() {
-  player.insertPlayer();
-  enemy.insertEnemy();
-  timeMovePlayer = setInterval(() => player.move(), 5);
-  timeMoveEnemy = setInterval(() => enemy.move(), 5);
+let bullet
+
+function createEnemy() {
+    let randomCoordX = Math.floor(Math.random() * 10) * 80
+    let enemy = new Enemy(randomCoordX, 0) 
+    enemy.insertEnemy()
+    enemyArray.push(enemy)
 }
 
-window.addEventListener("keydown", function (event) {
-  switch (event.key) {
-    case "a":
-      player.direction = -1;
-      break;
-    case "d":
-      player.direction = 1;
-      break;
-  }
-});
+function startGame() {
+    player.insertPlayer()
+    timerMovePlayer = setInterval(() => player.move(), 10)
+    timerCreateEnemy = setInterval(() => createEnemy(), 1800)
+}
 
-window.addEventListener("keyup", function (event) {
-  player.direction = 0;
-});
+//Key Listener
+window.addEventListener('keydown', function (e) {
+    switch (e.key) {
+        case 'a':
+            player.direction = -1
+            break
+        case 'd':
+            player.direction = 1
+            break
+        case 'w':
+            bullet = new Bullet(enemyArray)
+            bullet.insertBullet() 
+            break
+    }
+})
 
-startGame();
+window.addEventListener('keyup', function (e) {
+    player.direction = 0
+})
+
+button.addEventListener("click",() => {
+    console.log("Press")
+    board.style.display = "block"
+    screen.style.display= "none"
+    startGame()
+})
